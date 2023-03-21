2ae72762c5f7351436e76d5ed69c7e6366737d84
Harry Watts <98606501+HarryMadn3ss@users.noreply.github.com>
2023-03-21 18:39:10 +0000

    fixed bob

--- Assets/enemyController.cs
+++ Assets/enemyController.cs
@@ -5,43 +5,74 @@ using UnityEngine;
 public class enemyController : MonoBehaviour
 {
     public BoxCollider2D boxCollider;
+    public CapsuleCollider2D capsuleCollider;
     public Rigidbody2D rb;
-    public bool left = true;
+    public Rigidbody2D playerRB;
+       
+    bool facingRight = true;
+    int direction = 1;
     public float speed = 10;
+
+    float maxVelocity = 3;
 
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(left)
+        if(facingRight)
         {
-            rb.AddForce(new Vector2(-speed, 0), ForceMode2D.Force);
-            transform.localScale = new Vector3(-1, 1, 1);
+            direction = 1;
         }
-        if(!left)
+        else
         {
-            rb.AddForce(new Vector2(speed, 0), ForceMode2D.Force);
-            transform.localScale = new Vector3(1, 1, 1);
+            direction = -1;
         }
+
+        rb.AddForce(new Vector2(speed * direction, 0), ForceMode2D.Force);            
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+
+        if(rb.velocity.x == 0)
+        {
+            rb.AddForce(new Vector2(speed * direction, 5));
+            Debug.Log("Panik!");
+        }
+            
+        
+        //if(facingRight)
+        //{
+        //    rb.AddForce(new Vector2(speed, 0), ForceMode2D.Force);            
+        //    rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+        //    transform.localScale = new Vector3(1, 1, 1);
+        //}
     }
 
     private void OnCollisionExit2D(Collision2D boxCollider)
     {
-        if(boxCollider != null)
+        transform.Rotate(0, 180, 0);
+        if (facingRight)
         {
-            if(left)
-            {
-                left = false;
-            }
-            else
-            {
-                left = true;
-            }
+            facingRight = false;            
+        }
+        else
+        {
+            facingRight = true;            
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D capsuleCollider)
+    {
+        if (capsuleCollider.gameObject.CompareTag("Player") && playerRB.velocity.y < -1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    
 }
